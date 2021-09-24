bb8bceac92b1096423c001a6889c3b1a1f3d3991
gwanhyeong <wudy343@gmail.com>
2021-09-24 16:07:34 +0900

    use library(http) / stateful로 변경

--- network_sample/lib/ui/home_page.dart
+++ network_sample/lib/ui/home_page.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _title = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Network'),
+        title: const Text('Network'),
       ),
       body: Column(
         children: [
           ElevatedButton(
-            onPressed: () {},
-            child: Text('Fetch'),
+            onPressed: () {
+              var url =
+                  Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+              http.get(url).then((response) {
+                if (response.statusCode == 200) {
+                  final jsonResponse = convert.jsonDecode(response.body);
+                  setState(() {
+                    _title = jsonResponse['title'];
+                  });
+                }
+              });
+            },
+            child: const Text('Fetch'),
           ),
+          Text(_title),
         ],
       ),
     );
