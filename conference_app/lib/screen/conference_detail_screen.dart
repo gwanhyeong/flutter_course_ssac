788a98201055495353582713942642b927fb263a
gwanhyeong <wudy343@gmail.com>
2021-10-05 20:21:47 +0900

    feat: url_launcher 적용 및 os 권한 추가

--- conference_app/lib/screen/conference_detail_screen.dart
+++ conference_app/lib/screen/conference_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:conference_app/model/conference.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ConferenceDetailScreen extends StatelessWidget {
   final Conference data;
@@ -12,7 +13,36 @@ class ConferenceDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(data.name ?? ''),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(data.location ?? ''),
+            const SizedBox(height: 16),
+            Text('${data.start} ~ ${data.end}'),
+            if (data.link != null)
+              TextButton(
+                onPressed: _launchUrl,
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                ),
+                child: const Text(
+                  'Go to official website',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _launchUrl() async {
+    if (data.link == null) return;
+
+    await canLaunch(data.link!)
+        ? await launch(data.link!)
+        : throw Exception('Could not launch ${data.link}');
   }
 }
