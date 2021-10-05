bdfccd447da39fd5f9534263aaf4d2f8c1bcaa69
gwanhyeong <wudy343@gmail.com>
2021-10-05 16:40:58 +0900

    init: project 초기 생성

---
+++ conference_app/android/app/src/main/kotlin/com/gwanhyeong/conference_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gwanhyeong.conference_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ conference_app/lib/screen/conference_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConferenceListScreen extends StatelessWidget {
+  const ConferenceListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Conferences'),
+      ),
+      body: Container(),
+    );
+  }
+}
