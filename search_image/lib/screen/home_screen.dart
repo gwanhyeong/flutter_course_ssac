8b1e4991bc0366c11ebf872867b3ade418584476
gwanhyeong <wudy343@gmail.com>
2021-10-06 23:47:51 +0900

    feat: add SearchBar with action to HomeScreen

--- search_image/lib/screen/home_screen.dart
+++ search_image/lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:search_image/api.dart';
 import 'package:search_image/model/search_result.dart';
 import 'package:search_image/widget/image_item.dart';
+import 'package:search_image/widget/search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,19 +13,23 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   SearchResult? _searchResult;
+  late TextEditingController _controller;
+  late SnackBar _snackBar;
 
   @override
   void initState() {
     super.initState();
+    _controller = TextEditingController();
+    _snackBar = const SnackBar(
+      content: Text('검색할 키워드를 입력해주세요.'),
+    );
     _searchKeyword('iphone');
   }
 
-  void _searchKeyword(String keyword) async {
-    fetchSearchResult(keyword).then((SearchResult result) {
-      setState(() {
-        _searchResult = result;
-      });
-    });
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 
   @override
@@ -34,18 +39,51 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('API test'),
       ),
       body: SingleChildScrollView(
-        child: ListView.separated(
+        child: ListView(
           shrinkWrap: true,
-          itemCount: _searchResult != null ? _searchResult!.hits!.length : 0,
           physics: const NeverScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            return ImageItem(data: _searchResult!.hits![index]);
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(height: 6.0);
-          },
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 16.0,
+              ),
+              child: SearchBar(
+                controller: _controller,
+                onPressed: () {
+                  if (_controller.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+                    return;
+                  }
+
+                  _searchKeyword(_controller.text);
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+            ListView.separated(
+              shrinkWrap: true,
+              itemCount:
+                  _searchResult != null ? _searchResult!.hits!.length : 0,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ImageItem(data: _searchResult!.hits![index]);
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 6.0);
+              },
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _searchKeyword(String keyword) async {
+    fetchSearchResult(keyword).then((SearchResult result) {
+      setState(() {
+        _searchResult = result;
+      });
+    });
   }
 }
