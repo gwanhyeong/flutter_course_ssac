020dbe8987bd3a372f11d101dee74f35c152e6ac
gwanhyeong <wudy343@gmail.com>
2021-10-13 08:50:10 +0900

    init: 초기 앱 구조 잡기

---
+++ instagram_clone/lib/screen/root_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screen/tab_screen.dart';
+
+class RootScreen extends StatelessWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const TabScreen();
+  }
+}
