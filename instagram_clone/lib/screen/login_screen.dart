8aa2c0aba206b3fbe53dff20336795a8e541e7ba
gwanhyeong <wudy343@gmail.com>
2021-10-13 20:57:03 +0900

    feat: LoginScreen UI mockup

--- instagram_clone/lib/screen/login_screen.dart
+++ instagram_clone/lib/screen/login_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Text(
+                'Instagram Clone',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                ),
+              ),
+              const SizedBox(
+                height: 100.0,
+              ),
+              SignInButton(Buttons.Google, onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- instagram_clone/lib/screen/root_screen.dart
+++ instagram_clone/lib/screen/root_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/screen/tab_screen.dart';
+import 'package:instagram_clone/screen/login_screen.dart';
 
 class RootScreen extends StatelessWidget {
   const RootScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const TabScreen();
+    return const LoginScreen();
   }
 }
