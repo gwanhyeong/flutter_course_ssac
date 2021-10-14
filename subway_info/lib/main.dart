c9bd711142aef5468e20d234bdf078e7d16fdd14
gwanhyeong <wudy343@gmail.com>
2021-10-14 17:16:25 +0900

    init: project 초기 생성

---
+++ subway_info/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('실시간 지하철 정보'),
+      ),
+      body: Container(),
+    );
+  }
+}
